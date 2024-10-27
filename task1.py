59c2c0e700a08998277edcb035011ecb103207ba
anastasiaderzhypilska <itnastia04@gmail.com>
2024-10-28 08:24:01 +1300

    task1.py

--- task1.py
+++ task1.py
@@ -20,8 +20,7 @@ def likes(names):
         others = count - 2
         return names[0] + ", " + names[1] + " and " + str(others) + " others like this"
 
-name = input ('Enter your name')
-print(likes([name]))
+
 
 print(likes([]))  
 print(likes(["Peter"]))  
